a5d1caec8c4407ddb0a96102a40a71a715ca6315
Maximus <hey@m4xy.org>
2025-03-18 15:38:43 +0530

    chngs

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -19,17 +19,15 @@ export const Hero = () => {
       id="Home"
       className="h-[492px] md:h-[800px] flex items-center justify-center overflow-hidden relative [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]"
     >
-      <Balatro isRotate={false} mouseInteraction={false} pixelFilter={2000} />
+      <Balatro isRotate={false} mouseInteraction={false} pixelFilter={2000} spinRotation={-2.0} spinSpeed={7.0} />
 
       <div className="container absolute flex flex-col items-center justify-center h-full text-center">
         {/* Animated Image */}
-        
         <div
           className={`h-72 w-72 flex items-center justify-center transition-opacity duration-1000 ${showContent ? "opacity-0" : "animate-scalePulse"}`}
         >
           <Image src="/images/Heist_Card.png" alt="Heist Card" width={1920} height={1080} className="mx-auto" />
         </div>
-
         {/* Fade-in Content */}
         <div
           className={`transition-all absolute duration-1000 delay-500 flex flex-col items-center justify-center h-full ${showContent ? "opacity-100 translate-y-2" : "opacity-0 translate-y-5"}`}
